a470657c5a0e1a127327b000642106fbcbced8be
Alex <yuhengchen9@gmail.com>
2021-11-13 20:30:48 +0800

    Virtual Data

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+    return {
+        products: [
+            { id: 1, name: "Kayak", categroy: "Watersports", description: "", price: 275 },
+            { id: 2, name: "Lifejacket", categroy: "Watersports", description: "", price: 48.95 },
+            { id: 3, name: "Soccer Ball", categroy: "Soccer", description: "", price: 19.50 },
+            { id: 4, name: "Corner Flags", categroy: "Soccer", description: "", price: 34.95 },
+            { id: 5, name: "Stadium", categroy: "Soccer", description: "", price: 79500 },
+            { id: 6, name: "Thinking Cap", categroy: "Chess", description: "", price: 16 },
+            { id: 7, name: "Unsteady Chair", categroy: "Chess", description: "", price: 29.95 },
+            { id: 8, name: "Human Chess Board", categroy: "Chess", description: "", price: 75 },
+            { id: 9, name: "Bling Bling King", categroy: "Chess", description: "", price: 1200 },
+        ],
+        orders: []
+    }
+}
